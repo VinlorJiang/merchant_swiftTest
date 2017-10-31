51e7c40683bd8256bdd025ccf663c4732c362774
Vinlor <13824380425@163.com>
2017-10-31 15:25:03 +0800

    创建启动图

---
+++ merchantPlatform/Classes/Other/Resource/Adv/VLAdvertViewController.swift
@@ -0,0 +1,28 @@
+//
+//  VLAdvertViewController.swift
+//  merchantPlatform
+//
+//  Created by dinpay on 2017/10/31.
+//  Copyright © 2017年 com.vinlor. All rights reserved.
+//
+
+import UIKit
+
+class VLAdvertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+        self.view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
